de0ed2e9ba0367d71cd128bf5b5c320496feb400
julydeath <manojkarajada.mk@gmail.com>
2023-04-14 14:53:06 +0530

    final ui

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -2,14 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://localhost:27017/netflix", {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
