61bf0b64c060c572ffcc5b79f355c007be14d89d
julydeath <manojkarajada.mk@gmail.com>
2023-04-14 15:07:34 +0530

    final: netflix- wi with movie backend

--- netflix-ui/src/index.js
+++ netflix-ui/src/index.js
@@ -4,6 +4,9 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import { store } from "./store";
+import axios from "axios";
+
+axios.defaults.baseURL = "netflix-production.up.railway.app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
